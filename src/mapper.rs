0abdbe1555ebc04b7a81fa4cf9faeaef6b9c6903
Alex Vergara <pyzyryab@tutanota.com>
2021-09-15 18:08:04 +0200

    Cleaning src folder

--- src/mapper.rs
+++
@@ -1,9 +0,0 @@
-use tokio_postgres::Row;
-
-/// Sets the way of how to deserialize a custom type T
-/// from a Row object retrieved from a database query
-pub trait RowMapper<T>: Sized {
-
-    /// Deserializes a database Row result into Self
-    fn deserialize(row: &Row) -> T;
-}
