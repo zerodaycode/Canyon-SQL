aef22211aa1d055e1e7e3371acebdd5684fe797d
Gonzalo Busto Musi <gonzalo.busto@gmail.com>
2022-03-12 04:05:00 +0100

    Added support for remote host. Normalize database connection file name to 'secrets.toml'.

--- canyon_sql/src/connector.rs
+++ canyon_sql/src/connector.rs
@@ -2,15 +2,16 @@ use tokio_postgres::{Client, Connection, Error, NoTls, Socket, tls::NoTlsStream}
 use std::{fs, collections::HashMap, marker::PhantomData};
 
 /// Manages to retrieve the credentials to the desired database connection from an
-/// handcoded `Secrets.toml` file, located at the root of the project.
+/// handcoded `secrets.toml` file, located at the root of the project.
 pub struct DatabaseCredentials {
     username: String,
     password: String,
-    db_name: String,
+    host: String,
+    db_name: String
 }
 
 impl DatabaseCredentials{
-    
+
     pub fn new() -> Self {
 
         let parsed_credentials = DatabaseCredentials::credentials_parser();
@@ -18,15 +19,16 @@ impl DatabaseCredentials{
         Self {
             username: parsed_credentials.get("username").unwrap().to_owned(),
             password: parsed_credentials.get("password").unwrap().to_owned(),
+            host:parsed_credentials.get("host").unwrap().to_owned(),
             db_name: parsed_credentials.get("db_name").unwrap().to_owned()
         }
     }
 
     pub fn credentials_parser() -> HashMap<String, String> {
-    
-        const FILE_NAME: &str = "Secrets.toml";
+
+        const FILE_NAME: &str = "secrets.toml";
         let mut credentials_mapper: HashMap<_, _> = HashMap::new();
-        
+
         let secrets_file = fs::read_to_string(FILE_NAME)
             .expect( // TODO Convert this to a custom error
                 &(format!(
@@ -39,7 +41,7 @@ impl DatabaseCredentials{
             .split_terminator("\n");
 
         for entry in secrets_file_splitted {
-            let cleaned_entry = 
+            let cleaned_entry =
                 entry
                 .split_ascii_whitespace()
                 .filter(
@@ -53,17 +55,17 @@ impl DatabaseCredentials{
 
             let attr = pair.get(0).unwrap();
             let value = pair.get(1).unwrap();
-            
+
             credentials_mapper.insert(attr.to_owned(), value.to_owned());
         }
-        
+
         credentials_mapper
     }
 }
 
 /// Creates a new connection with a database, returning the necessary tools
 /// to query the created link.
-/// TODO: Explain how to use this struct independently from CRUD trait 
+/// TODO: Explain how to use this struct independently from CRUD trait
 pub struct DatabaseConnection<'a> {
     pub client: Client,
     pub connection: Connection<Socket, NoTlsStream>,
@@ -82,9 +84,10 @@ impl<'a> DatabaseConnection<'a> {
         let (new_client, new_connection) =
             tokio_postgres::connect(
             &format!(
-                "postgres://{user}:{pswd}@localhost/{db}",
-                    user = credentials.username, 
-                    pswd = credentials.password, 
+                "postgres://{user}:{pswd}@{host}/{db}",
+                    user = credentials.username,
+                    pswd = credentials.password,
+                    host = credentials.host,
                     db = credentials.db_name
                 )[..], 
             NoTls)
