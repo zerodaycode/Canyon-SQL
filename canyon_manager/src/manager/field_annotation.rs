8951b06429752bdb4a096531499cbb0a5cd09371
Alex Vergara <pyzyryab@tutanota.com>
2022-04-10 22:28:21 +0200

    Arguments on foreign key now are Named Punctuated values with identifiers instead of a raw string

--- canyon_manager/src/manager/field_annotation.rs
+++ canyon_manager/src/manager/field_annotation.rs
@@ -1,6 +1,6 @@
-use std::convert::TryFrom;
-use proc_macro2::Ident;
-use syn::Attribute;
+use std::{convert::TryFrom, collections::HashMap};
+use proc_macro2::{Ident, Span};
+use syn::{Attribute, Token, punctuated::Punctuated, MetaNameValue};
 
 /// The available annotations for a field that belongs to any struct
 /// annotaded with `#[canyon_entity]`
@@ -11,55 +11,74 @@ pub enum EntityFieldAnnotation {
 
 impl EntityFieldAnnotation {
 
-    pub fn new(ident: Ident, foreign_key: String) -> Self {
-        let table_column_tuple = Self::foreign_key_parser(ident, foreign_key);
-        Self::ForeignKey(
-            table_column_tuple.clone().expect("Table value in attribute").0, 
-            table_column_tuple.clone().expect("Column value in attribute").1
-        )
+    pub fn new(attr_args: Result<Punctuated<MetaNameValue, Token![,]>, syn::Error>) -> Result<Self, syn::Error> {
+        Self::foreign_key_parser(attr_args)
     }
 
     pub fn get_as_string(&self) -> String {
         match &*self {
             Self::ForeignKey(table, column) => 
-                format!("Table: {}, Column: {}",table,column)
+                format!("Table: {}, Column: {}", table, column)
         }
     }
 
-    pub fn foreign_key_parser(ident: Ident, foreign_key: String) -> Result<(String, String), syn::Error> {
-        
-        let table_column:Vec<String> = foreign_key.split(",")
-            .map(|x|x.trim().to_string())
-            .collect();
+    pub fn foreign_key_parser(attr_args: Result<Punctuated<MetaNameValue, Token![,]>, syn::Error>) -> Result<Self, syn::Error> {
+        match attr_args {
+            Ok(name_value) => {
+                let mut data: HashMap<String, String> = HashMap::new();
 
-        let mut table = String::new();
-        let mut column = String::new();
-        
-        if table_column.iter().any(|c| c.starts_with("column")) && 
-            table_column.iter().any(|c| c.starts_with("table"))
-        {
-            table.push_str(table_column.iter()
-                .find(|&t|t.starts_with("table"))
-                .unwrap()
-                .split("=").collect::<Vec<&str>>().get(1).unwrap()
-                .trim()
-            );
+                for nv in name_value {
+                    // The identifier
+                    let attr_value_ident = nv.path.get_ident().unwrap().to_string();
+                    // The value after the Token[=]
+                    let attr_value = match nv.lit {
+                        // Error if the token is not a string literal
+                        syn::Lit::Str(v) => v.value(),
+                        _ => {
+                            return Err(
+                                syn::Error::new_spanned(
+                                    nv.path.clone(), 
+                                    format!("Only string literals are supported for the `{}` attribute", attr_value_ident)
+                                )
+                            )
+                        }
+                    };
+                    data.insert(attr_value_ident, attr_value);
+                }
 
-            column.push_str(table_column.iter()
-                .find(|&t|t.starts_with("column"))
-                .unwrap()
-                .split("=").collect::<Vec<&str>>().get(1).unwrap()
-                .trim()
-            );
-
-            Ok((table, column))
-        } else {
-            Err(
-                syn::Error::new_spanned(
-                    ident,
-                    "Malformed values on the {:?}`"
+                Ok(
+                    EntityFieldAnnotation::ForeignKey(
+                        match data.get("table") {
+                            Some(table) => table.to_owned(),
+                            None => {
+                                return Err(
+                                    syn::Error::new_spanned(
+                                            Ident::new("Table", Span::call_site()), 
+                                        format!("Missed `table` argument on the Foreign Key annotation")
+                                    )
+                                )
+                            },
+                        }, 
+                        match data.get("column") {
+                            Some(table) => table.to_owned(),
+                            None => {
+                                return Err(
+                                    syn::Error::new_spanned(
+                                            Ident::new("Column", Span::call_site()), 
+                                        format!("Missed `column` argument on the Foreign Key annotation")
+                                    )
+                                )
+                            },
+                        }, 
+                    )
                 )
-            )
+            },
+            Err(_) => return Err(
+                syn::Error::new(
+                        Span::call_site(), 
+                    format!("Error generating the Foreign Key")
+                )
+            ),
         }
     }
 }
@@ -70,16 +89,16 @@ impl TryFrom<&&Attribute> for EntityFieldAnnotation {
 
     fn try_from(attribute: &&Attribute) -> Result<Self, Self::Error> {
 
-        let (ident, _args) = (
-            attribute.path.segments[0].ident.clone(),
-            attribute.parse_args::<syn::LitStr>()
-                .unwrap()
-                .value()
-        );
+        let name_values: Result<Punctuated<MetaNameValue, Token![,]>, syn::Error> = 
+            attribute.parse_args_with(Punctuated::parse_terminated);
 
+
+        let ident = attribute.path.segments[0].ident.clone();
         Ok(
-            match ident.to_string().as_str() {
-                "foreign_key" => EntityFieldAnnotation::new(ident, _args),
+            match ident.clone().to_string().as_str() {
+                "foreign_key" => 
+                    EntityFieldAnnotation::new(name_values)
+                        .expect("Error generating the Foreign Key details"),
                 _ => {
                     return Err(
                         syn::Error::new_spanned(
@@ -92,4 +111,3 @@ impl TryFrom<&&Attribute> for EntityFieldAnnotation {
         )
     }
 }
-
