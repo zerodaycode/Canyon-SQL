61d5fcbacd9d9af7325520259967a9587869ddaa
Alex Vergara <alejandromarc@ext.inditex.com>
2022-09-13 16:53:00 +0200

    To trait object the Err variant on the Result of the DatabaseConnection type

--- canyon_connection/src/postgresql_connector.rs
+++ canyon_connection/src/postgresql_connector.rs
@@ -15,25 +15,38 @@ unsafe impl Send for DatabaseConnection<'_> {}
 unsafe impl Sync for DatabaseConnection<'_> {}
 
 impl<'a> DatabaseConnection<'a> {
+    pub async fn new(datasource: &DatasourceProperties<'_>) -> Result<DatabaseConnection<'a>, Box<(dyn std::error::Error + Send + Sync + 'static)>> {
+        match datasource.db_type {
+            "postgresql" => {
+                let (new_client, new_connection) =
+                    tokio_postgres::connect(
+                    &format!(
+                        "postgres://{user}:{pswd}@{host}/{db}",
+                            user = datasource.username,
+                            pswd = datasource.password,
+                            host = datasource.host,
+                            db = datasource.db_name
+                        )[..], 
+                    NoTls
+                    ).await?;
 
-    pub async fn new(datasource: &DatasourceProperties<'_>) -> Result<DatabaseConnection<'a>, Error> {
-        let (new_client, new_connection) =
-            tokio_postgres::connect(
-            &format!(
-                "postgres://{user}:{pswd}@{host}/{db}",
-                    user = datasource.username,
-                    pswd = datasource.password,
-                    host = datasource.host,
-                    db = datasource.db_name
-                )[..], 
-            NoTls
-            ).await?;
-
-        Ok(Self {
-            client: new_client,
-            connection: new_connection,
-            phantom: &PhantomData
-        })
+                Ok(Self {
+                    client: new_client,
+                    connection: new_connection,
+                    phantom: &PhantomData
+                })
+            },
+            "sqlserver" => todo!(),
+            &_ => return Err(
+                std::io::Error::new(
+                    std::io::ErrorKind::Unsupported,
+                    format!(
+                        "There's no `{}` database supported in Canyon-SQL", 
+                        datasource.db_type
+                    )
+                ).into_inner().unwrap()
+            )
+        }
     }
 }
 
