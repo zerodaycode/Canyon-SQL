e88714fb0d51eb7f80a29ff6a697d4de5ad4f9ba
Alex Vergara <alejandromarc@ext.inditex.com>
2022-09-13 17:55:11 +0200

    Base implementation of a multi-database connection handler

--- canyon_connection/src/postgresql_connector.rs
+++ canyon_connection/src/postgresql_connector.rs
@@ -1,21 +1,33 @@
-use std::marker::PhantomData;
-use tokio_postgres::{Client, Connection, Error, NoTls, Socket, tls::NoTlsStream};
+use async_std::net::TcpStream;
+
+use tiberius::{Config, AuthMethod};
+use tokio_postgres::{Client, Connection, NoTls, Socket, tls::NoTlsStream};
 
 use crate::datasources::DatasourceProperties;
 
 
-/// Represents a connection with a `PostgreSQL` database
-pub struct DatabaseConnection<'a> {
+/// A connection with a `PostgreSQL` database
+pub struct PostgreSqlConnection {
     pub client: Client,
-    pub connection: Connection<Socket, NoTlsStream>,
-    pub phantom: &'a PhantomData<DatabaseConnection<'a>>
+    pub connection: Connection<Socket, NoTlsStream>
 }
 
-unsafe impl Send for DatabaseConnection<'_> {}
-unsafe impl Sync for DatabaseConnection<'_> {}
+/// A connection with a `SqlServer` database
+pub struct SqlServerConnection {
+    pub client: tiberius::Client<TcpStream>
+}
 
-impl<'a> DatabaseConnection<'a> {
-    pub async fn new(datasource: &DatasourceProperties<'_>) -> Result<DatabaseConnection<'a>, Box<(dyn std::error::Error + Send + Sync + 'static)>> {
+/// The Canyon database connection handler.
+pub struct DatabaseConnection {
+    pub postgres_connection: Option<PostgreSqlConnection>,
+    pub sqlserver_connection: Option<SqlServerConnection>
+}
+
+unsafe impl Send for DatabaseConnection {}
+unsafe impl Sync for DatabaseConnection {}
+
+impl DatabaseConnection {
+    pub async fn new(datasource: &DatasourceProperties<'_>) -> Result<DatabaseConnection, Box<(dyn std::error::Error + Send + Sync + 'static)>> {
         match datasource.db_type {
             "postgresql" => {
                 let (new_client, new_connection) =
@@ -31,12 +43,45 @@ impl<'a> DatabaseConnection<'a> {
                     ).await?;
 
                 Ok(Self {
-                    client: new_client,
-                    connection: new_connection,
-                    phantom: &PhantomData
+                    postgres_connection: Some(PostgreSqlConnection {
+                        client: new_client,
+                        connection: new_connection
+                    }),
+                    sqlserver_connection: None
                 })
             },
-            "sqlserver" => todo!(),
+            "sqlserver" => {
+                let mut config = Config::new();
+
+                config.host("ecomt.database.windows.net");
+                config.port(1433);
+                config.database("OteaCenterDes");
+
+                // Using SQL Server authentication.
+                config.authentication(AuthMethod::sql_server("administrador", "ny0crzlp@"));
+
+                // on production, it is not a good idea to do this
+                config.trust_cert();
+
+                // Taking the address from the configuration, using async-std's
+                // TcpStream to connect to the server.
+                let tcp = TcpStream::connect(config.get_addr()).await
+                    .ok().expect("Error instanciating the SqlServer TCP Stream");
+
+                // We'll disable the Nagle algorithm. Buffering is handled
+                // internally with a `Sink`.
+                tcp.set_nodelay(true).ok().expect("Error in the SqlServer `nodelay` config");
+
+                // Handling TLS, login and other details related to the SQL Server.
+                let client = tiberius::Client::connect(config, tcp).await;
+
+                Ok(Self {
+                    postgres_connection: None,
+                    sqlserver_connection: Some(SqlServerConnection {
+                        client: client.ok().expect("Fallo en la conexión a la BBDD")
+                    })
+                })
+            },
             &_ => return Err(
                 std::io::Error::new(
                     std::io::ErrorKind::Unsupported,
